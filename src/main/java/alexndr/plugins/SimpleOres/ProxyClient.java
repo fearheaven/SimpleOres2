216db75f1922cec7752dcd6274f909342abf19e7
Sinhika <sinhika@republicofnewhome.org>
2016-05-26 17:46:54 -0500

    fixed missing textures

--- src/main/java/alexndr/plugins/SimpleOres/ProxyClient.java
+++ src/main/java/alexndr/plugins/SimpleOres/ProxyClient.java
@@ -3,21 +3,30 @@ import alexndr.api.helpers.game.RenderItemHelper;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 
 public class ProxyClient extends ProxyCommon 
 {
+	RenderItemHelper renderHelper = new RenderItemHelper(SimpleOres.plugin);
+	
 	@Override
 	public void preInit(FMLPreInitializationEvent event) 
 	{
 		super.preInit(event);
+		if(event.getSide() == Side.CLIENT) 
+		{
+			renderHelper.addBowRenderDetails(Content.mythril_bow);
+			renderHelper.addBowRenderDetails(Content.onyx_bow);
+			renderHelper.renderItemsAndBlocks();
+			renderHelper.renderItemStuff(event);
+		}
 	} // end preInit
 	
 	@Override
     public void load(FMLInitializationEvent event)
     {
 		super.load(event);
-		RenderItemHelper.renderItemsAndBlocks(SimpleOres.plugin);
     } // end load
     
 	@Override
